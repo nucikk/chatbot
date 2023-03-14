99f495f62c22784fa2ac089f30cd4cde35f8979f
nuci <kirianuci@gmail.com>
2023-03-14 20:34:47 +0400

    Add feature to display host user messages in new items

--- chat/src/Page/HostChat.jsx
+++ chat/src/Page/HostChat.jsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../Style/chat.css"
 // import NextIcon from "../Image/next-Icon.svg"
 import SendeBtn from "../Image/Send-btn.svg"
 import { MessageStatus } from '../Components/MessageStatus'
 const HostChat = () => {
-  const [hostMessage, setHostMessage] = useState('');
-  const [messageSent, setMessageSent] = useState(false);
+  const [hostMessage, setHostMessage] = useState("");
+  const [messageSent, setMessageSent] = useState("");
+  const [newMessages, setNewMessages] = useState([]);
 
-  const HostName = "host"
-  const FrameName = "Frame"
+  const HostName = "host";
+  const FrameName = "Frame";
+
+  useEffect(() => {
+    const savedMessage = localStorage.getItem("message");
+    if (savedMessage) {
+      setHostMessage(savedMessage);
+    }
+  }, []);
 
   const handleHostMessageChange = (e) => {
-    setHostMessage(e.target.value);
+    setMessageSent(e.target.value);
   };
 
   const handleSendClick = (e) => {
-    setMessageSent(true);
-    setHostMessage('');
+    e.preventDefault();
+    if (messageSent.trim() !== "") { // შამოწმებს რომ ცარიელი მესიჯი არ გაიგზავნოს
+      setHostMessage(messageSent);
+      setMessageSent("");
+      setNewMessages(prevMessages => [...prevMessages, messageSent])
+      localStorage.setItem("message", messageSent);
+    }
   };
+
   return (
     <>
       <div className="chat_container">
@@ -28,20 +42,29 @@ const HostChat = () => {
 
             <div className="chat">
               <div className='host_msg'>
-                <div className="notification_host">
-                  {messageSent && <p>{hostMessage}</p>}
-                </div>
+                {newMessages.map((message, index) => (
+                  <div className="notification_host" key={index}>
+                    <p>{message}</p>
+                  </div>
+                ))}
               </div>
               <div className='frame_msg'>
                 <div className="notification_frame">
                   Lorem, ipsum.
                 </div>
                 <div className="input_box">
-                  <input className="msg_input" type="text" placeholder="Type a message" value={hostMessage} onChange={handleHostMessageChange} />
-                  <img className="send_btn" src={SendeBtn} alt="send message icon" onClick={handleSendClick} />
+
+                  <input
+                    className="msg_input"
+                    type="text"
+                    placeholder="Type a message"
+                    value={messageSent}
+                    onChange={handleHostMessageChange}
+                  />
+                  <button className="send_btn" type="submit" onClick={handleSendClick}><img src={SendeBtn} alt="send message icon" /></button>
+
                 </div>
               </div>
-
             </div>
           </div>
 
@@ -52,16 +75,27 @@ const HostChat = () => {
 
             <div className="chat">
               <div className='host_msg'>
-                <div className="notification_host">Lorem, ipsum dolor.</div>
-              </div>
-              <div className='frame_msg'>
                 <div className="notification_frame">
                   Lorem, ipsum.
                 </div>
 
+
+              </div>
+              <div className='frame_msg'>
+                {newMessages.map((message, index) => (
+                  <div className="notification_host" key={index}>
+                    <p>{message}</p>
+                  </div>
+                ))}
                 <div className="input_box">
-                  <input className="msg_input" type="text" placeholder="Type a message" />
-                  <img className="send_btn" src={SendeBtn} alt="send message icon" />
+                  <input
+                    className="msg_input"
+                    type="text"
+                    placeholder="Type a message"
+                    value={messageSent}
+                    onChange={handleHostMessageChange}
+                  />
+                  <button className="send_btn" type="submit" onClick={handleSendClick}><img src={SendeBtn} alt="send message icon" /></button>
                 </div>
               </div>
             </div>
